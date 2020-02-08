37d3b676450c7dba5002f2029644988b8cdeb1a4
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-02-08 15:48:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/advancedanimotion/Step5Activity.kt
@@ -0,0 +1,12 @@
+package com.example.advancedanimotion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Step5Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step5)
+    }
+}
