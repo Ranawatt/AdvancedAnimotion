d73b7f031c07e7f123a801526913b33a3221475a
Sugandh Kumar <sugandhpatna95@gmail.com>
2021-05-02 17:22:31 +0530

    Added array of images in drawables

--- app/src/main/java/com/example/advancedanimotion/Step9CompletedActivity.kt
+++ app/src/main/java/com/example/advancedanimotion/Step9CompletedActivity.kt
@@ -4,6 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class Step9CompletedActivity : AppCompatActivity() {
+
+    var images = intArrayOf(
+        R.drawable.bryce_canyon,
+        R.drawable.cathedral_rock,
+        R.drawable.death_valley,
+        R.drawable.fitzgerald_marine_reserve,
+        R.drawable.goldengate,
+        R.drawable.golden_gate_bridge,
+        R.drawable.shipwreck_1,
+        R.drawable.shipwreck_2,
+        R.drawable.grand_canyon,
+        R.drawable.horseshoe_bend,
+        R.drawable.muir_beach,
+        R.drawable.rainbow_falls,
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_step9_completed)
