0e3c58a7c991f901d9849c73b4ad9d59e8a29dd1
Sugandh Kumar <sugandhpatna95@gmail.com>
2021-05-02 17:07:57 +0530

    Implemented Carousel Motion

--- app/src/main/java/com/example/advancedanimotion/Step9Activity.kt
+++ app/src/main/java/com/example/advancedanimotion/Step9Activity.kt
@@ -1,11 +1,47 @@
 package com.example.advancedanimotion
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.helper.widget.Carousel
+import com.google.android.material.card.MaterialCardView
 
 class Step9Activity : AppCompatActivity() {
+
+    var colors = intArrayOf(
+        Color.parseColor("#ffd54f"),
+        Color.parseColor("#ffca28"),
+        Color.parseColor("#ffc107"),
+        Color.parseColor("#ffb300"),
+        Color.parseColor("#ffa000"),
+        Color.parseColor("#ff8f00"),
+        Color.parseColor("#ff6f00"),
+        Color.parseColor("#c43e00")
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_step9)
+        setupCarousel()
+    }
+
+    private fun setupCarousel() {
+        val carousel = findViewById<Carousel>(R.id.carousel) ?: return
+        val numImages = colors.size
+
+        carousel.setAdapter(object : Carousel.Adapter {
+            override fun count(): Int {
+                return numImages
+            }
+
+            override fun populate(view: View, index: Int) {
+                if (view is MaterialCardView) {
+                    view.setBackgroundColor(colors[index])
+                }
+            }
+
+            override fun onNewItem(index: Int) {
+            }
+        })
     }
 }
